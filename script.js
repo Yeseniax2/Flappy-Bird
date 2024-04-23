ce9ad7d6feb33cfe802a549d3a7ba136b81ff66b
yesenia <yeseniab743@gmail.com>
2024-04-23 10:59:13 -0400

    added let functions

---
+++ script.js
@@ -0,0 +1,21 @@
+let move_speed = 3, gravity = 0.5;
+let bird = document.querySelector('.mainbird');
+let img = document.getElementById('bird-1');
+
+let bird_props = bird.getBoundingClientRect();
+
+let background = document.querySelector('.background').getBoundingClientRect();
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'start';
+img.style.display = 'none';
+Message.classList.add('messageStyle');
+
+document.addEventListener('keydown', (e) => {
+     if(e.key == 'Enter' && game_state i= 'play'){
+        document.querySelectorAll('.pipe_sprite').forEach((e)) => {}
+
+     }
+})
